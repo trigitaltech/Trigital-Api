1b8ca4515d10d18d974df19cf6d8149d3086be02
manojanandilla <manojanand432@gmail.com>
2015-11-02 18:59:27 +0530

    Telnet command executor for cpe and default

--- src/main/java/com/trigital/cm/service/MACDetailsService.java
+++ src/main/java/com/trigital/cm/service/MACDetailsService.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.trigital.cm.domain.CPEMacDetails;
+import com.trigital.cm.domain.CPETelnetMacDetails;
 import com.trigital.cm.domain.CountersMacDetails;
 import com.trigital.cm.domain.DefaultMACDetails;
+import com.trigital.cm.domain.DefaultTelnetMACDetails;
 import com.trigital.cm.domain.MACDetails;
 import com.trigital.cm.domain.PHYMacDetails;
 import com.trigital.cm.domain.QOSMacDetails;
@@ -24,12 +26,15 @@ public class MACDetailsService {
 
 	@Autowired
 	ExecuteShellComand esc;
+	
+	@Autowired
+	TelnetCommandExecutor tce;
 
 	String executeCommandReset, executeCommandDefault, executeCommandCpe,
 			executeCommandQos, executeCommandsWideband, executeCommandPhy,
 			executeCommandCounters = null;
 
-	// MACDetails resultReset = new MACDetails();
+	// cisco10k
 	DefaultMACDetails defaultMACDetails = null;
 	CPEMacDetails cpeMacDetails = null;
 	QOSMacDetails macDetailsQosA, macDetailsQosB = null;
@@ -37,6 +42,14 @@ public class MACDetailsService {
 	PHYMacDetails phyMacDetails = null;
 	CountersMacDetails countersMacDetails = null;
 
+	//CASA
+	DefaultTelnetMACDetails defaultTelnetMACDetails = null;
+	CPETelnetMacDetails cpeTelnetMacDetails = null;
+	
+	
+	
+	
+	
 	List<QOSMacDetails> listOfQOSMacDetails = new ArrayList<QOSMacDetails>();
 
 	String[] defaultCommandResults, cpeCommandResults, qosCommandResults,
@@ -50,15 +63,14 @@ public class MACDetailsService {
 
 	public MACDetails getMACDetails(MACDetails macDetails) {
 
-		if (macDetails.getCommand() == "RESET" && macDetails.getCommand() == "") {
+		if (macDetails.getCommand().equals("RESET")  && macDetails.getIpDetails().getCmts_make().equals("Cisco10K")) {
 
 			executeCommandReset = "clear cable modem "
 					+ macDetails.getMac_Address() + " reset";
 			System.out.println(executeCommandReset);
 			esc.shellCommandExecuter(executeCommandReset);
 
-		} else if (macDetails.getCommand() == "ALL"
-				&& macDetails.getCommand() == "") {
+		} else if (macDetails.getCommand().equals("ALL") && macDetails.getIpDetails().getCmts_make().equals("Cisco10K")) {
 
 			// Default Command
 			defaultMACDetails = this.executeDefaultMacDetails(macDetails.getIpDetails().getIp_Address(), macDetails.getMac_Address());
@@ -77,15 +89,20 @@ public class MACDetailsService {
 			
 			//Counters Command
 			countersMacDetails = this.executeCountersMacDetails(macDetails.getIpDetails().getIp_Address(), macDetails.getMac_Address());
-			
-			macDetails.setDefaultMACDetails(defaultMACDetails);
-			macDetails.setCpeMacDetails(cpeMacDetails);
-			macDetails.setPhyMacDetails(phyMacDetails);
-			macDetails.setWidebandMacDetails(widebandMacDetails);
-			macDetails.setListofQosMacDetails(listOfQOSMacDetails);
-			macDetails.setCountersMacDetails(countersMacDetails);
 
+		} else if (macDetails.getCommand().equals("ALL") && macDetails.getIpDetails().getCmts_make().equals("CASA")) {
+			
+			executeCommandDefault = "shcm ";
+			tce.executeTelnetCommand(executeCommandDefault);
 		}
+		
+		macDetails.setDefaultMACDetails(defaultMACDetails);
+		macDetails.setCpeMacDetails(cpeMacDetails);
+		macDetails.setPhyMacDetails(phyMacDetails);
+		macDetails.setWidebandMacDetails(widebandMacDetails);
+		macDetails.setListofQosMacDetails(listOfQOSMacDetails);
+		macDetails.setCountersMacDetails(countersMacDetails);
+		
 		return macDetails;
 	}
 
@@ -107,6 +124,22 @@ public class MACDetailsService {
 
 		return defaultMACDetails;
 	}
+	
+	// Default Telnet Command
+	public DefaultTelnetMACDetails executeTelnetDefaultMacDetails(String macAddress) {
+		
+		executeCommandDefault = "shcm "+ macAddress;
+		System.out.println(executeCommandDefault);
+
+		defaultCommandResults = tce.executeTelnetCommand(executeCommandDefault).split("\n")[2].split("\\s+");
+
+		defaultTelnetMACDetails = new DefaultTelnetMACDetails(defaultCommandResults[0], defaultCommandResults[1],
+				defaultCommandResults[3], defaultCommandResults[4], defaultCommandResults[5], defaultCommandResults[6],
+				defaultCommandResults[7], defaultCommandResults[8], defaultCommandResults[8], defaultCommandResults[9],
+				defaultCommandResults[10]);
+
+		return defaultTelnetMACDetails;
+	}
 
 	// CPE Command
 	public CPEMacDetails executeCPEMacDetails(String ipAddress,String macAddress) {
@@ -123,6 +156,21 @@ public class MACDetailsService {
 				cpeCommandResults[3]);
 
 		return cpeMacDetails;
+	}
+	
+	// CPE Command
+	public CPETelnetMacDetails executeTelnetCPEMacDetails(String macAddress) {
+
+		
+		executeCommandCpe = "shcm "+ macAddress + " cpe";
+		
+		System.out.println(executeCommandCpe);
+		
+		cpeCommandResults = tce.executeTelnetCommand(executeCommandCpe).split("\n")[1].split("\\s+");
+		
+		cpeTelnetMacDetails = new CPETelnetMacDetails(cpeCommandResults[0], cpeCommandResults[1], cpeCommandResults[2], cpeCommandResults[3], cpeCommandResults[4]);
+
+		return cpeTelnetMacDetails;
 	}
 	
 	// QOS Command
--- src/main/java/com/trigital/cm/service/TelnetCommandExecutor.java
+++ src/main/java/com/trigital/cm/service/TelnetCommandExecutor.java
@@ -1,27 +1,66 @@
 package com.trigital.cm.service;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.apache.commons.net.telnet.TelnetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.jscape.inet.telnet.Telnet;
+import com.jscape.inet.telnet.TelnetAdapter;
+import com.jscape.inet.telnet.TelnetException;
+import com.jscape.inet.telnet.TelnetScript;
+import com.jscape.inet.telnet.TelnetSession;
+import com.jscape.inet.telnet.TelnetTask;
+
 @Service
-public class TelnetCommandExecutor {
+public class TelnetCommandExecutor extends TelnetAdapter{
 	
 	private static final Logger log = LoggerFactory.getLogger(TelnetCommandExecutor.class);
 	
+    private Telnet telnet = null;
+   
+	
 	public String executeTelnetCommand(String executeCommand) {
-		final TelnetCommandExecutor esc = new TelnetCommandExecutor();
 		
-		final PropertyManager propertyManager = new PropertyManager();
+		String loginPrompt = "Antophill-CMTS login:";
+        String login = "venkat";
+        String passwordPrompt = "Antophill-CMTS password:";
+        String password = "venkat@75";
+        String shellPrompt = "Antophill-CMTS>";
+        String hostname = "125.99.88.90";
+        TelnetSession session = new TelnetSession(hostname);
+        String result = null;
+        
+		session.setShellPrompt(shellPrompt);
+		session.setLoginPrompt(loginPrompt);
+		session.setPasswordPrompt(passwordPrompt);
+		session.addTelnetListener(this);
+        
+		try {
+			
+			session.connect(login,password);
+			System.out.println("successfully connected to telnet server");
+			
+			result = session.send(executeCommand);
+			System.out.println("successfully executed the show cable modem command");
+			
+			System.out.println(result);
+
+			session.disconnect();
+			System.out.println("successfully disconnected the server");
+		} catch (TelnetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		//"rsh -l root 125.99.127.30 shcm a089.e439.abc4"
-		return esc.commandExecutor(executeCommand);
+        
+		return result;
 
-	}
-
-	private String commandExecutor(String executeCommand) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
